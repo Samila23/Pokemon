c0f3a2987cfb0c8f3e9f1240287484b46be41338
SAMILA MILENA DIAS DE PAULA <60451467+Samila23@users.noreply.github.com>
2023-05-07 14:01:04 -0300

    Conclusao-testes ConsultaInformacoes e Batalha 2

---
+++ pokemon/src/test/java/com/pokemon/pokemon/consultaInformacoes/consultaInformacoesTests.java
@@ -0,0 +1,33 @@
+package com.pokemon.pokemon.consultaInformacoes;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+@ExtendWith(SpringExtension.class)																			//Informa que é uma classe de testes 
+@AutoConfigureMockMvc																						//Sempre que usar o MockMVC
+@SpringBootTest
+public class consultaInformacoesTests {
+	@Autowired
+	private MockMvc mockMvc;	
+	
+	@Test
+	void getOneTest() throws Exception{
+		ResultActions response = mockMvc.perform (
+				get("/consultaInformacoes/pikachu")
+				.contentType("application/json")
+				);
+		MvcResult result = response.andReturn();
+		String responseStr = result.getResponse().getContentAsString();
+		System.out.println(responseStr);
+
+	}
+}
